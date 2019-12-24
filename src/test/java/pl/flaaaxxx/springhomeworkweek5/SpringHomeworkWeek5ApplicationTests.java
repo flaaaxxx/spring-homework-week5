2827b072e5c861608f916e65aed263fded6245b2
flaaaxxx <flaaaxxx@gmail.com>
2019-12-24 21:27:02 +0100

    new

---
+++ src/test/java/pl/flaaaxxx/springhomeworkweek5/SpringHomeworkWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.flaaaxxx.springhomeworkweek5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHomeworkWeek5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
